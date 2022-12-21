a6421f6642acf24a442a1424c5a01b1f0aff8795
Sena Serefoglu <sena.serefoglu.9@hotmail.com>
2022-12-21 18:53:56 +0300

    Importing libraries and cleaning the datasets

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+# Importing the dataset
+testDB = pd.read_csv('test.csv')
+trainDB = pd.read_csv('train.csv')
+
+# Dropping the columns that are not needed
+testDB = testDB.drop(['Ticket', 'Cabin', 'Fare', 'Embarked', 'Name'], axis=1)
+trainDB = trainDB.drop(['Ticket', 'Cabin', 'Fare', 'Embarked', 'Name'], axis=1)
+
+# Replacing the NaN values with the mean of the column
+testDB["Age"] = testDB["Age"].replace(np.NaN, testDB["Age"].mean())
+trainDB["Age"] = trainDB["Age"].replace(np.NaN, trainDB["Age"].mean())
